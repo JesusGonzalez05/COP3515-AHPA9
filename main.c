4f98aed9fd241bf51659cbb8fec95428739d5085
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-28 17:09:29 -0400

    commented out program to  make easier to program.

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main (){
+    // initial tank capacities
+
+    // simulate 12 cars arriving (you could loop indefinitely or stop with user input)
+
+
+    // serve car: reduce 10 gallons from the selected tank
+    // check if tank needs refill
+    // print current tank status
+    // simulate a car arriving at a random pump
+    // simulate requesting a refill truck
+    // print the status of all tanks
+
+
+
 
     return 0;
 }
